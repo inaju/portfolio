192180b45a161535bed03d31952b99ffc9bc838a
inaju <mitchelinajuo@gmail.com>
2023-01-11 17:43:33 +0100

    working on the header

---
+++ src/data/index.ts
@@ -0,0 +1,100 @@
+const data = {
+    name: 'Mitchel Inaju',
+    image: '/projects/mitchel_inaju.jpg',
+    // image: 'https://avatars.githubusercontent.com/u/76690419?v=4',
+    username: 'mitchelinaju',
+    keywords:
+        'Mitchel, inaju, nextjs, Reactjs,web dev, blogger, content creator',
+    profession: 'Frontend web developer',
+    intro: 'Focused on building products that people love to use. I love to learn new things and share my knowledge with others. I teach others by creating content!',
+    about: 'Im a software developer building cool products and teaching others by creating content and giving talks that revolves around many areas in tech.',
+    // I’m a 15 y/o full-stack web3 developer building cool products and teaching others by creating content that revolves around many areas in web3.',
+    // website: 'https://www.avneesh.tech',
+    projects: [
+        {
+            name: 'Grazac Academy',
+            image: '/projects/grazac_academy.png',
+            about: 'Grazac Academy gives you the opportunity to learn in-demand skills to build your technology career.',
+            tech: ['SASS', 'Reactjs', 'Javascript', 'Html', 'CSS'],
+            links: {
+                GitHub: '',
+
+                Live: 'https://grazacacademy.com/',
+            },
+
+            role: 'Frontend Developer',
+        },
+        {
+            name: 'Zwilt Application',
+            image: '/projects/zwilt_application.png',
+            about: 'Zwilt is a leading Talent Acquisition Suite that makes it easy for talent teams to connect companies with top talent. Zwilt Application helps to collect relevant data that showcases the talents skill and proficiency which helps out in the job application process',
+            tech: ['Material UI', 'Javascript', 'ReactJs', 'Html', 'CSS'],
+            links: {
+                Live: 'https://zwilt-application-frontend.vercel.app/',
+            },
+            role: 'Frontend Developer',
+        },
+        {
+            name: 'Keepup',
+            image: '/projects/keepup.png',
+            about: 'Cloud based Note taking application that enables professionals sync their notes across platforms, guaranteed to boot your productivity',
+            tech: [
+                'ReactJs',
+                'Firebase',
+                'Firestore',
+                'Javascript',
+                'Tailwind', 'Html', 'CSS'
+            ],
+            links: {
+                GitHub: 'https://github.com/inaju/keepup',
+                Live: 'https://keepup-63ced.firebaseapp.com/',
+            },
+            role: 'Fullstack developer',
+        },
+        {
+            name: 'Vtuslot',
+            image: '/projects/vtuslot.png',
+            about: 'Africa’s first VTU trading platform,Buy and sell on a simple and safe platform with trusted users. Get Started Market view. Our trusted partners.',
+            tech: ['ReactJs', 'Html', 'CSS'],
+            links: {
+                GitHub: 'https://github.com/inaju/vtuslot',
+                Live: 'https://vtuslot.vercel.app/',
+            },
+            role: 'Frontend developer',
+        },
+    ],
+    links: [
+        {
+            name: 'GitHub',
+            icon: 'github.svg',
+            link: 'https://github.com/inaju',
+        },
+        {
+            name: 'Twitter',
+            icon: 'twitter.svg',
+            link: 'https://twitter.com/mitchelinaju',
+        },
+        {
+            name: 'Hashnode',
+            icon: 'hashnode.svg',
+            link: 'https://mitchel.hashnode.dev',
+        },
+        {
+            name: 'Instagram',
+            icon: 'instagram.svg',
+            link: 'https://www.instagram.com/inaju.mitchel/',
+        },
+        {
+            name: 'Mail me',
+            icon: 'mail.svg',
+            link: 'mailto:mitchelinajuo@gmail.com',
+        },
+        {
+            name: 'LinkedIn',
+            icon: 'linkedin.svg',
+            link: 'https://www.linkedin.com/in/mitchel-inaju',
+        },
+    ],
+};
+
+export default data;
